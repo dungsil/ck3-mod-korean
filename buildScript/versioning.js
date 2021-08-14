c5eb6383f898cd97365764efffeb72c496250eb2
김용건 (Kim Younggeon) <mail@kyg.kr>
2021-08-15 00:36:18 +0900

    ci: add deploy config

---
+++ buildScript/versioning.js
@@ -0,0 +1,13 @@
+﻿const versionRegex = /^version="((([0-9])\.([0-9])\.([0-9]))(?:-([0-9A-Za-z-.]+))?(?:\+([0-9A-Za-z-.]+))?)"/
+
+function readVersion (contents) {
+  return versionRegex.exec(contents)[1]
+}
+
+function writeVersion (contents, nextVersion) {
+  const currentVersion = readVersion(contents)
+  return contents.replace(currentVersion, nextVersion)
+}
+
+module.exports.readVersion = readVersion
+module.exports.writeVersion = writeVersion
